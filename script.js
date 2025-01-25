8d8302a5db09ae44b6081051c588d866ddc5ef76
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-01-25 23:38:26 +0530

    ADDED BUTTON TO SHOW TABLE

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 const promise = fetch("https://dummyjson.com/users");
+const btn = document.querySelector("button");
 
-promise.then(function(rawData){
+btn.addEventListener("click",()=>{
+
+    promise.then(function(rawData){
     rawData.json().then(function(finalData){
         console.log(finalData);
 
 
-        const listWrapper = document.querySelector(".list");
+        // const listWrapper = document.querySelector(".list");
+        // listWrapper.className = "bg-warning"
 
-        if (!listWrapper) {
-            console.error("List wrapper not found!");
-            return;
-        }
+        // if (!listWrapper) {
+        //     console.error("List wrapper not found!");
+        //     return;
+        // }
     
         const users = finalData.users;
 
-        for(let i =0;i<users.length;i++){
-            const liEle = document.createElement("li");
-            liEle.textContent = users[i].firstName +" "+ users[i].lastName;
-            listWrapper.append(liEle);
-        }
+        // for(let i =0;i<users.length;i++){
+        //     const liEle = document.createElement("li");
+        //     const imgEle = document.createElement("img");
+        //     imgEle.src = users[i].image;
+        //     imgEle.alt = "Placeholder Image"; 
+        //     imgEle.width = 150;              
+        //     imgEle.height = 150; 
+        //     listWrapper.appendChild(imgEle);
+        //     liEle.textContent = users[i].firstName +" "+ users[i].lastName ; 
+        //     listWrapper.append(liEle);
+        // }
 
         createTable(users);
-    });
-})
-.catch(function(error){
-    console.error(error);
-});
 
+
+    });
+    })
+    .catch(function(error){
+        console.error(error);
+    });
+
+})
 
 
 function createTable(data) {
@@ -38,12 +51,14 @@ function createTable(data) {
     }
 
     const table = document.createElement("table");
-    table.className = "table table-bordered table-striped border border-3";//bootstrap classes for table
+
+    //bootstrap classes for table
+    table.className = "table table-bordered table-striped border border-3";
 
     //table header row
 
     const headerRow = document.createElement("tr");
-    const headers = ["ID" , "First Name" , "Last Name" ,"Age" , "Email","Phone"];
+    const headers = ["ID" , "First Name" , "Last Name" ,"Age" , "Email", "Phone", "University"];
 
     headers.forEach((header) =>{
         const th = document.createElement("th");
@@ -58,7 +73,7 @@ function createTable(data) {
     data.forEach((user) =>{
         const row = document.createElement("tr");
 
-        const cells = [user.id,user.firstName,user.lastName,user.age,user.email,user.phone];
+        const cells = [user.id,user.firstName,user.lastName,user.age,user.email,user.phone,user.university];
 
         cells.forEach((cell)=>{
             const td = document.createElement("td");
